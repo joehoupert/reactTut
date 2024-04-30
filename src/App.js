aeb8b7a097b5d27a4508a4802cdbd50c9d097125
dustinthelight <46585024+joehoupert@users.noreply.github.com>
2024-04-30 17:39:12 +0000

    Let's do some O's

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    //if you're taken (ie !NULL), don't get took
+    if (squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    // nextSquares[i] = "X";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
